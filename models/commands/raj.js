8534fd7df71f873a4bce2fff023e93d0084d43a1
rajahmed404 <mdsohidul86145@gmail.com>
2025-06-22 12:07:45 +0600

    Update raj.js

--- models/commands/raj.js
+++ models/commands/raj.js
@@ -1,26 +1,41 @@
-const fs = require("fs");
+const axios = require("axios");
+
 module.exports.config = {
   name: "raj",
-    version: "1.0.1",
+  version: "1.0.1",
   hasPermssion: 0,
   credits: "JOY", 
   description: "hihihihi",
   commandCategory: "no prefix",
   usages: "tea",
-    cooldowns: 5, 
+  cooldowns: 5, 
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  if (event.body.indexOf("Assalamu alaikum")==0 || event.body.indexOf("assalamualaikum")==0 || event.body.indexOf("আসসালামু আলাইকুম")==0 || event.body.indexOf("আসসালামু আলাইকুম")==0) {
-    var msg = {
+module.exports.handleEvent = async function({ api, event }) {
+  const { threadID, messageID, body } = event;
+
+  if (body.toLowerCase().startsWith("assalamu alaikum") || 
+      body.toLowerCase().startsWith("assalamualaikum") ||
+      body.startsWith("আসসালামু আলাইকুম")) {
+
+    try {
+      const imgURL = "https://i.imgur.com/5Qa7zUs.jpeg"; // এখানে আপনার Imgur লিংক বসান
+      const response = await axios.get(imgURL, { responseType: "stream" });
+
+      const msg = {
         body: "ওয়ালাইকুম সালাম",
-        attachment: fs.createReadStream(__dirname + `/JOY/raj.png`)
-      }
+        attachment: response.data
+      };
+
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+      api.setMessageReaction("🖤", messageID, () => {}, true);
+
+    } catch (err) {
+      console.error("Image fetch failed:", err);
     }
   }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
+};
 
-  }
+module.exports.run = function({ api, event }) {
+  // No command usage needed
+};
