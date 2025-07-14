3479adc94f4ce6e2849b33f46f57d60d70411708
rajahmed404 <mdsohidul86145@gmail.com>
2025-07-14 19:08:18 +0600

    Update hack.js

--- models/commands/hack.js
+++ models/commands/hack.js
@@ -1,88 +1,103 @@
-const axios = require("axios");
-const fs = require("fs-extra");
-const { loadImage, createCanvas } = require("canvas");
-
-module.exports = {
-  config: {
-    name: "hack",
-    version: "1.0.0",
-    author: "Joy",
-    role: 0,
-    shortDescription: "Example image command",
-    longDescription: "Create a fun image using avatar and text",
-    category: "fun",
-    guide: "{pn} [@mention or leave blank]"
+module.exports.config = {
+  name: "hack",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "JOY",
+  description: "hack",
+  commandCategory: "hack",
+  usages: "@mention",
+  dependencies: {
+        "axios": "",
+        "fs-extra": ""
   },
+  cooldowns: 0
+};
 
-  wrapText: function (ctx, text, maxWidth) {
-    return new Promise(resolve => {
-      if (ctx.measureText(text).width < maxWidth) return resolve([text]);
-      if (ctx.measureText('W').width > maxWidth) return resolve(null);
-      const words = text.split(' ');
-      const lines = [];
-      let line = '';
-      while (words.length > 0) {
-        let split = false;
-        while (ctx.measureText(words[0]).width >= maxWidth) {
-          const temp = words[0];
-          words[0] = temp.slice(0, -1);
-          if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
-          else {
-            split = true;
-            words.splice(1, 0, temp.slice(-1));
-          }
-        }
-        if (ctx.measureText(`${line}${words[0]}`).width < maxWidth) line += `${words.shift()} `;
+module.exports.wrapText = (ctx, name, maxWidth) => {
+  return new Promise(resolve => {
+    if (ctx.measureText(name).width < maxWidth) return resolve([name]);
+    if (ctx.measureText('W').width > maxWidth) return resolve(null);
+    const words = name.split(' ');
+    const lines = [];
+    let line = '';
+    while (words.length > 0) {
+      let split = false;
+      while (ctx.measureText(words[0]).width >= maxWidth) {
+        const temp = words[0];
+        words[0] = temp.slice(0, -1);
+        if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
         else {
-          lines.push(line.trim());
-          line = '';
+          split = true;
+          words.splice(1, 0, temp.slice(-1));
         }
-        if (words.length === 0) lines.push(line.trim());
       }
-      return resolve(lines);
-    });
-  },
+      if (ctx.measureText(`${line}${words[0]}`).width < maxWidth) line += `${words.shift()} `;
+      else {
+        lines.push(line.trim());
+        line = '';
+      }
+      if (words.length === 0) lines.push(line.trim());
+    }
+    return resolve(lines);
+  });
+} 
 
-  onStart: async function ({ api, event, usersData }) {
-    const pathImg = __dirname + "/cache/hack_bg.png";
-    const pathAvt = __dirname + "/cache/hack_avt.png";
+module.exports.run = async function ({ args, Users, Threads, api, event, Currencies }) {
+  const { loadImage, createCanvas } = require("canvas");
+  const fs = global.nodemodule["fs-extra"];
+  const axios = global.nodemodule["axios"];
+  let pathImg = __dirname + "/cache/background.png";
+  let pathAvt1 = __dirname + "/cache/Avtmot.png";
 
-    const id = Object.keys(event.mentions)[0] || event.senderID;
-    const name = await usersData.getName(id);
 
-    const backgroundURL = "https://drive.google.com/uc?id=1RwJnJTzUmwOmP3N_mZzxtp63wbvt9bLZ";
-    const avatarURL = `https://graph.facebook.com/${id}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+  var id = Object.keys(event.mentions)[0] || event.senderID;
+  var name = await Users.getNameUser(id);
+  var ThreadInfo = await api.getThreadInfo(event.threadID);
 
-    const bgData = (await axios.get(backgroundURL, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(pathImg, Buffer.from(bgData, "utf-8"));
+  var background = [
 
-    const avtData = (await axios.get(avatarURL, { responseType: "arraybuffer" })).data;
-    fs.writeFileSync(pathAvt, Buffer.from(avtData, "utf-8"));
+    "https://i.imghippo.com/files/qEl8678STs.png"
+];
+  var rd = background[Math.floor(Math.random() * background.length)];
 
-    const baseImg = await loadImage(pathImg);
-    const avatarImg = await loadImage(pathAvt);
+  let getAvtmot = (
+    await axios.get(
+      `https://graph.facebook.com/${id}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
+      { responseType: "arraybuffer" }
+    )
+  ).data;
+  fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
 
-    const canvas = createCanvas(baseImg.width, baseImg.height);
-    const ctx = canvas.getContext("2d");
+  let getbackground = (
+    await axios.get(`${rd}`, {
+      responseType: "arraybuffer",
+    })
+  ).data;
+  fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
 
-    ctx.drawImage(baseImg, 0, 0, canvas.width, canvas.height);
+  let baseImage = await loadImage(pathImg);
+  let baseAvt1 = await loadImage(pathAvt1);
 
+  let canvas = createCanvas(baseImage.width, baseImage.height);
+  let ctx = canvas.getContext("2d");
+  ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
     ctx.font = "400 23px Arial";
     ctx.fillStyle = "#1878F3";
     ctx.textAlign = "start";
 
+
     const lines = await this.wrapText(ctx, name, 1160);
-    ctx.fillText(lines.join("\n"), 200, 497);
+    ctx.fillText(lines.join('\n'), 200,497);//comment
+    ctx.beginPath();
 
-    ctx.drawImage(avatarImg, 83, 437, 100, 101);
 
-    const finalBuffer = canvas.toBuffer();
-    fs.writeFileSync(pathImg, finalBuffer);
-    fs.removeSync(pathAvt);
+  ctx.drawImage(baseAvt1, 83, 437, 100, 101);
 
-    return api.sendMessage({
-      body: "😈 Hack complete!",
-      attachment: fs.createReadStream(pathImg)
-    }, event.threadID, () => fs.unlinkSync(pathImg), event.messageID);
-  }
-};
+  const imageBuffer = canvas.toBuffer();
+  fs.writeFileSync(pathImg, imageBuffer);
+  fs.removeSync(pathAvt1);
+  return api.sendMessage({ body: ` `, attachment: fs.createReadStream(pathImg) },
+      event.threadID,
+      () => fs.unlinkSync(pathImg),
+      event.messageID);
+    }
