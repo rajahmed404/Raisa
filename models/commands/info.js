536986fba85360f1cb0516be2beaae1cd1c35e62
rajahmed404 <mdsohidul86145@gmail.com>
2025-07-21 21:13:45 +0600

    Update info.js

--- models/commands/info.js
+++ models/commands/info.js
@@ -6,14 +6,13 @@ module.exports.config = {
   version: "1.0.0",
   permission: 0,
   credits: "Joy Ahmed",
-  prefix: true,
   description: "ব্যক্তিগত তথ্য বক্স",
-  category: "prefix",
+  commandCategory: "prefix",
   usages: "",
   cooldowns: 5,
 };
 
-module.exports.run = async function ({ api, event }) {
+module.exports.run = async function({ api, event }) {
   const fbUID = "100080837633857";
   const imgURL = `https://graph.facebook.com/${fbUID}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
   const imgPath = __dirname + "/cache/info.png";
@@ -25,7 +24,7 @@ module.exports.run = async function ({ api, event }) {
 🧕🏻 ইসলাম | মেয়ে | বয়স: 𝟭𝟲+
 🎂 জন্মদিন: ০২ মার্চ ২০০৮
 📚 ছাত্র | 💔 অবিবাহিত
-❤️ রিলেশন: Single ☺️
+❤️ রিলেশন: সিংগেল ☺️
 💌 ভালোবাসে: বিড়াল 🐱, বই 📚, গান 🎶
 🩸 রক্তের গ্রুপ: B+
 🧠 প্রিয় বিষয়: জীববিজ্ঞান
@@ -35,14 +34,16 @@ module.exports.run = async function ({ api, event }) {
 🔗 প্রোফাইল: fb.com/${fbUID}
 ╰╼|━━━━━━━━━━━━━━|╾╯`;
 
-  request(encodeURI(imgURL)).pipe(fs.createWriteStream(imgPath)).on("close", () => {
-    api.sendMessage(
-      {
-        body: msg,
-        attachment: fs.createReadStream(imgPath),
-      },
-      event.threadID,
-      () => fs.unlinkSync(imgPath)
+  request(encodeURI(imgURL))
+    .pipe(fs.createWriteStream(imgPath))
+    .on("close", () =>
+      api.sendMessage(
+        {
+          body: msg,
+          attachment: fs.createReadStream(imgPath),
+        },
+        event.threadID,
+        () => fs.unlinkSync(imgPath)
+      )
     );
-  });
 };
