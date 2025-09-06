952e21513cdbcaeb61939be26549a3764054a30c
rajahmed404 <mdsohidul86145@gmail.com>
2025-09-06 14:19:50 +0600

    Update uid.js

--- models/commands/uid.js
+++ models/commands/uid.js
@@ -1,41 +1,53 @@
-const fs = require("fs-extra");
-const request = require("request");
-
 module.exports.config = {
-  name: "uid",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "JOY | Modified by ChatGPT",
-  description: "Get User ID with profile picture",
-  commandCategory: "Tools",
-  cooldowns: 5
+	name: "uid",
+	aliases: ["getuid"],
+	version: "1.0.0",
+	permission: 0,
+	credits: "Nayan",
+	prefix: true,
+	description: "get user id.",
+	category: "without prefix",
+	cooldowns: 5
 };
 
-module.exports.run = async function({ api, event, Users }) {
-  const mentions = event.mentions;
-  const sendUIDWithPic = async (uid, name) => {
-    const imgPath = __dirname + `/cache/${uid}.jpg`;
-    const picURL = `https://graph.facebook.com/${uid}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+module.exports.run = async function({ event, api, args, client, Currencies, Users, utils, __GLOBAL, reminder }) {
+const fs = global.nodemodule["fs-extra"];
+    const request = global.nodemodule["request"];
+    const axios = global.nodemodule['axios']; 
+    if(event.type == "message_reply") { 
+      let name = await Users.getNameUser(event.messageReply.senderID) 
+	uid = event.messageReply.senderID
+	var callback = () =>   api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${uid}\n[ ▶️]➜ 𝗜𝗕: m.me/${uid}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${uid}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+        () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+    return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+        () => callback()); 
+    }
+    if (!args[0]) {
+      var uid = event.senderID;
+ 
+        var callback = () =>  api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${event.senderID}\n[ ▶️]➜ 𝗜𝗕: m.me/${event.senderID}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${event.senderID}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+        () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+    return request(encodeURI(`https://graph.facebook.com/${event.senderID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+        () => callback()); 
+    }
+    else {
+	if (args[0].indexOf(".com/")!==-1) {
+    const res_ID = await api.getUID(args[0]);
     
-    request(encodeURI(picURL))
-      .pipe(fs.createWriteStream(imgPath))
-      .on("close", () => {
-        api.sendMessage({
-          body: `👤 𝗡𝗮𝗺𝗲: ${name}\n🆔 𝗨𝗜𝗗: ${uid}\n🔗 𝗟𝗶𝗻𝗸: https://facebook.com/${uid}`,
-          attachment: fs.createReadStream(imgPath)
-        }, event.threadID, () => fs.unlinkSync(imgPath), event.messageID);
-      });
-  };
-
-  // No mentions → self UID
-  if (Object.keys(mentions).length === 0) {
-    const uid = event.senderID;
-    const name = await Users.getNameUser(uid);
-    return sendUIDWithPic(uid, name);
-  }
-
-  // Mentions → get each person's UID
-  for (const [uid, name] of Object.entries(mentions)) {
-    await sendUIDWithPic(uid, name.replace("@", ""));
-  }
-};
+    
+    var callback = () => api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${res_ID}\n[ ▶️]➜ 𝗜𝗕: m.me/${res_ID}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${res_ID}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+        () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+    return request(encodeURI(`https://graph.facebook.com/${res_ID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+        () => callback()); }
+	else {
+		if (args.join().indexOf('@') !== -1) 
+      var uid = Object.keys(event.mentions) 
+      var callback = () => 
+api.sendMessage({body:`=== [ 𝗨𝗜𝗗 𝗨𝗦𝗘𝗥 ] ====\n━━━━━━━━━━━━━━━━━━\n[ ▶️]➜ 𝗜𝗗: ${uid}\n[ ▶️]➜ 𝗜𝗕: m.me/${uid}\n[ ▶️]➜ 𝗟𝗶𝗻𝗸𝗳𝗯: https://www.facebook.com/profile.php?id=${uid}\n━━━━━━━━━━━━━━━━━━`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+        () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+    return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+        () => callback()); 
+               
+	}
+}
+}
